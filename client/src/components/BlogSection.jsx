1b4a346b9e5cc4bc234233ddeec19b28300cfc7a
Jayash Saini <jayashysaini7361@gmail.com>
2024-07-10 22:29:54 +0530

    perfom(UI): optimized show more loader

--- client/src/components/BlogSection.jsx
+++ client/src/components/BlogSection.jsx
@@ -7,15 +7,16 @@ import { MutatingDots } from "react-loader-spinner";
 const BlogSection = ({ blogsType }) => {
   // Destructure blogsType from props
   const [blogs, setBlogs] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [page, setPage] = useState(1);
+  const [showMoreLoader, setShowMoreLoader] = useState(false);
   const [limit, setLimit] = useState(6);
   const [isShowMore, setIsShowMore] = useState(false);
 
   const blogsHandler = useCallback(async () => {
     await requestHandler(
       async () => await getAllBlogs(blogsType, page, limit),
-      setIsLoading,
+      setShowMoreLoader,
       (data) => {
         setBlogs((prev) => {
           const currentBlogs = [...prev, ...data.data.blogs];
@@ -32,11 +33,10 @@ const BlogSection = ({ blogsType }) => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setIsLoading(true);
       if (blogsType === "favorites") {
         await requestHandler(
           async () => await getFavoritesBlog(page, limit),
-          setIsLoading,
+          setShowMoreLoader,
           (data) => {
             setBlogs((prev) => {
               const currentBlogs = [...prev, ...data.data.blogs];
@@ -52,7 +52,7 @@ const BlogSection = ({ blogsType }) => {
       } else if (blogsType === "myBlogs") {
         await requestHandler(
           async () => await getMyBlogs(page, limit),
-          setIsLoading,
+          setShowMoreLoader,
           (data) => {
             setBlogs((prev) => {
               const currentBlogs = [...prev, ...data.data.blogs];
@@ -99,19 +99,34 @@ const BlogSection = ({ blogsType }) => {
           <BlogCard key={blog._id} data={blog} />
         ))}
       </div>
-      {isShowMore && (
-        <div className="w-full mt-20 flex items-center justify-center ">
-          <button
-            className=" py-2 px-5 border-[#2e8f83] border-[1px] text-base text-white hover:text-[#12332f] rounded hover:bg-[#34a394] duration-150 ease-in-out"
-            onClick={() => {
-              setLimit((prev) => prev + 6);
-              setPage((prev) => prev + 1);
-            }}
-          >
-            Show more
-          </button>
-        </div>
-      )}
+      {isShowMore &&
+        (showMoreLoader ? (
+          <div className="w-full h-[300px] flex items-center justify-center">
+            <MutatingDots
+              visible={true}
+              height="80"
+              width="80"
+              color="#004445"
+              secondaryColor="#2c786c"
+              radius="12.5"
+              ariaLabel="mutating-dots-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+            />
+          </div>
+        ) : (
+          <div className="w-full mt-20 flex items-center justify-center ">
+            <button
+              className=" py-2 px-5 border-[#2e8f83] border-[1px] text-base text-white hover:text-[#12332f] rounded hover:bg-[#34a394] duration-150 ease-in-out"
+              onClick={() => {
+                setLimit((prev) => prev + 6);
+                setPage((prev) => prev + 1);
+              }}
+            >
+              Show more
+            </button>
+          </div>
+        ))}
     </div>
   );
 };
